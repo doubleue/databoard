91b0db4a231d0d5fe0d5c2225cbc2133b783ab2d
l11040 <l1104033837@gmail.com>
2022-09-17 04:21:47 +0900

    Add : 공통 테마 추가
    - 모든 테마에서 공통으로 적용

---
+++ styles/commonTheme.ts
@@ -0,0 +1,15 @@
+const fontSize = {
+  xs: "0.75rem",
+  sm: "0.875rem",
+  base: "1rem",
+  lg: "1,125rem",
+  xl: "1.25rem",
+};
+
+const commonTheme = {
+  fontSize,
+};
+
+export type FontSize = typeof fontSize;
+
+export default commonTheme;
