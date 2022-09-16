289513aa442d980bbd3809a4c3d85ed8897735b0
l11040 <l1104033837@gmail.com>
2022-09-17 04:01:50 +0900

    Update : title 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ function _app({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>socbol</title>
+        <title>DataBoard</title>
       </Head>
       <GlobalStyle />
       <Component {...pageProps} />
