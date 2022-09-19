ee0d8cb2807b6b2de6bdd377306b7aa3bcbee6a8
l11040 <l1104033837@gmail.com>
2022-09-19 20:30:53 +0900

    Add : SideBar 컴포넌트 추가

---
+++ components/SideBar/index.tsx
@@ -0,0 +1,14 @@
+interface SideBar {}
+
+export default function SideBar({}: SideBar) {
+  const menus = [
+    { id: "1", name: "메뉴 1" },
+    { id: "2", name: "Menu 2" },
+    { id: "3", name: "메뉴 3" },
+    { id: "4", name: "Menu 4" },
+  ];
+
+  return <>test</>;
+}
+
+import styled from "styled-components";
