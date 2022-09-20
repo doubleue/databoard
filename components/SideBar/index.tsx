ed385a55c7666c965226c5c4517f0d870936d19c
l11040 <l1104033837@gmail.com>
2022-09-20 22:42:24 +0900

    Update : SideBarItem 적용
    - 기존 Item이 같이 있던 형태에서 SideBarItem 이름으로 따로 컴포넌트를 추가하여 적용

--- components/SideBar/index.tsx
+++ components/SideBar/index.tsx
@@ -1,14 +1,22 @@
-interface SideBar {}
+import { ISideBarItem } from "../../types/side-bar";
+import SideBarItem from "./SideBarItem";
+import { Wrapper } from "./style";
 
-export default function SideBar({}: SideBar) {
-  const menus = [
-    { id: "1", name: "메뉴 1" },
-    { id: "2", name: "Menu 2" },
-    { id: "3", name: "메뉴 3" },
-    { id: "4", name: "Menu 4" },
+export interface SideBarProps {}
+
+export default function SideBar({}: SideBarProps) {
+  const menus: ISideBarItem[] = [
+    { id: "1", title: "메뉴 1" },
+    { id: "2", title: "Menu 2" },
+    { id: "3", title: "메뉴 3" },
+    { id: "4", title: "Menu 4" },
   ];
 
-  return <>test</>;
+  return menus ? (
+    <Wrapper>
+      {menus.map((item) => (
+        <SideBarItem key={item.id} title={item.title} id={item.id} />
+      ))}
+    </Wrapper>
+  ) : null;
 }
-
-import styled from "styled-components";
