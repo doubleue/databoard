ab4eefbe70cff19a00a765596c7daba9fc3eba0b
l11040 <l1104033837@gmail.com>
2022-09-22 03:13:34 +0900

    Add : MainLayout 추가

--- pages/dashboard/[id].tsx
+++ pages/dashboard/[id].tsx
@@ -1,7 +1,12 @@
 import { NextPage } from "next";
+import MainLayout from "../../components/layout/MainLayout";
 
 const Dashboard: NextPage<any> = ({}) => {
-  return <></>;
+  return (
+    <MainLayout title="Dashboard" seoTitle="Dashboard">
+      Dashboard
+    </MainLayout>
+  );
 };
 
 export default Dashboard;
