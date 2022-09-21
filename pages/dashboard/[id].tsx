785b153465a6404984ace8d0b9719cf12d9777c2
l11040 <l1104033837@gmail.com>
2022-09-22 05:03:33 +0900

    Add : api에서 id값에 해당하는 Item을 가져옴

--- pages/dashboard/[id].tsx
+++ pages/dashboard/[id].tsx
@@ -1,12 +1,27 @@
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
 import MainLayout from "../../components/layout/MainLayout";
+import { ISideBarItem } from "../../types/side-bar";
 
-const Dashboard: NextPage<any> = ({}) => {
+interface DashboardProps extends ISideBarItem {}
+
+const Dashboard: NextPage<any> = ({ id, title }: DashboardProps) => {
   return (
-    <MainLayout title="Dashboard" seoTitle="Dashboard">
-      Dashboard
+    <MainLayout title={title} seoTitle={title}>
+      {title}
     </MainLayout>
   );
+};
+
+const fetcher = (url: RequestInfo | URL) =>
+  fetch(url).then((res) => res.json());
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const data = await fetcher(
+    `http://localhost:3000/api/dashboard/${ctx?.params?.id}`
+  );
+  return {
+    props: data,
+  };
 };
 
 export default Dashboard;
