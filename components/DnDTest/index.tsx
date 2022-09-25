58ef6421e91721955d9658708110e55103748aef
l11040 <l1104033837@gmail.com>
2022-09-25 23:48:53 +0900

    Delete : react-beatiful-dnd 삭제
    - ssr 문제로 인한 해결방안이 마음에 들지 않아 패키지 변경

--- components/DnDTest/index.tsx
+++ components/DnDTest/index.tsx
@@ -1,24 +1,3 @@
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
-import styled from "styled-components";
-
-const ListWrapper = styled.div<{ isDraggingOver: boolean }>`
-  background: ${({ isDraggingOver }) => (isDraggingOver ? "blue" : "gray")};
-  width: 250px;
-  padding: grid;
-`;
-
 export default function DnDTest() {
-  return (
-    <DragDropContext onDragEnd={() => {}}>
-      <Droppable droppableId="droppable">
-        {(provided, snapshot) => (
-          <ListWrapper
-            {...provided.droppableProps}
-            ref={provided.innerRef}
-            isDraggingOver={snapshot.isDraggingOver}
-          ></ListWrapper>
-        )}
-      </Droppable>
-    </DragDropContext>
-  );
+  return <></>;
 }
