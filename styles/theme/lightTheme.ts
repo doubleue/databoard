ef4a06bf7da8fffb884bb17f9b611a90cce7cf61
l11040 <l1104033837@gmail.com>
2022-09-28 02:13:29 +0900

    Add : light, dark 테마 추가

--- styles/theme/lightTheme.ts
+++ styles/theme/lightTheme.ts
@@ -3,6 +3,27 @@ import commonTheme from "./commonTheme";
 
 const lightTheme: DefaultTheme = {
   ...commonTheme,
+  color: {
+    signature: {
+      main: "#0075F4",
+    },
+    font: {
+      main: "#2D2F36",
+      sub: "#424854",
+    },
+    background: {
+      main: "#F1F3F5",
+      card1: "#FFFEFE",
+      card2: "#FAFAFA",
+    },
+    icon: {
+      active: "#2D2F36",
+      inactive: "#B0B7C0",
+    },
+    button: {
+      main: "#F1F2F3",
+    },
+  },
 };
 
 export default lightTheme;
