d144752e2d63ab491e4527ad65cfbb5287722a34
l11040 <l1104033837@gmail.com>
2022-10-17 23:26:41 +0900

    Add : OS에서 색상 테마 변경시 연동되도록 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,30 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { useEffect, useState } from "react";
+import { useMediaQuery } from "react-responsive";
 import { RecoilRoot } from "recoil";
-import { ThemeProvider } from "styled-components";
+import { DefaultTheme, ThemeProvider } from "styled-components";
 import Modal from "../components/Modal";
 
 import GlobalStyle from "../styles/GlobalStyle";
+import darkTheme from "../styles/theme/darkTheme";
 import lightTheme from "../styles/theme/lightTheme";
 
 function _app({ Component, pageProps }: AppProps) {
+  const systemPrefers = useMediaQuery({
+    query: "(prefers-color-scheme: dark)",
+  });
+  const [theme, setTheme] = useState<DefaultTheme>(lightTheme);
+  useEffect(() => {
+    setTheme(systemPrefers ? darkTheme : lightTheme);
+  }, [systemPrefers]);
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>DataBoard</title>
       </Head>
-      <ThemeProvider theme={lightTheme}>
+      <ThemeProvider theme={theme}>
         <GlobalStyle />
         <RecoilRoot>
           <Modal />
